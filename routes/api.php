4ebc38095698e0fd1ea14077a4173db2efe995ee
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-10 14:06:18 +0000

    Adding Santcum and testing API with Postmane

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/', function () {
+    return "Hello World";
+});
