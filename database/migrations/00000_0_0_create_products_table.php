d6950004c873e16d35553b838094fe42b17a1aef
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-10 13:57:14 +0000

    Migration Done

---
+++ database/migrations/00000_0_0_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('price');  // Changed from int() to integer()
+            $table->integer('stock');  // Changed from int() to integer()
+            $table->string('status');
+            $table->integer('category_id');  // Changed from int() to integer()
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
