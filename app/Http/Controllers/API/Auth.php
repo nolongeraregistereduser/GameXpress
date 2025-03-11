9b3dd9daef30cf8aeb6cc72faaeb68ed0b079383
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-11 09:45:40 +0000

    Register Function Works Perfectly

--- app/Http/Controllers/API/Auth.php
+++ app/Http/Controllers/API/Auth.php
@@ -4,8 +4,41 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class Auth extends Controller
 {
-    //
+    
+  
+    public function register(Request $request)
+    {
+
+      $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => '0',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        return response()->json([
+            'status' => '200 Ok',
+            'message' => 'User created successfully',
+            'data' => $user
+        ]);
+    }
+
+
 }
