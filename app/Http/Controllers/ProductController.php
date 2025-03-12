9f6a3afadb7c23b1bf060bf2a8c0c5f28eb7c9fd
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-12 09:49:25 +0000

    adding validator namespace

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
 
 class ProductController extends Controller
 {
