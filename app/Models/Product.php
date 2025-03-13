ef4e3f6f20dc754aec147e8fbfa817fd1cb6b83c
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-13 09:38:11 +0000

    adding category model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,11 @@ class Product extends Model
 {
     protected $table = 'products';
     protected $fillable = ['name', 'slug','price', 'stock', 'status','category_id'];
+
+
+    
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
-
-
 // 'price, stock', 'status'   add this later for products 
