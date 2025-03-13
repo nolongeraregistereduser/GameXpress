694a40d66740057ee82f3812935b65ac3393f1a6
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-03-13 10:45:16 +0000

    Index function List All Categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -10,6 +11,19 @@ class CategoryController extends Controller
    
     public function index(){
 
+
+        $user = Auth::user();
+
+        if($user->hasRole('super_admin') || $user->hasRole('product_manager')){
+
+         return response()->json([
+            'status' => '200 Ok',
+            'message' => 'Welcome Super Admin',
+            'data' => Category::all()
+        ]);
+        }
+
+        else return 'Unauthorized Access';
 
         return response()->json([
             'message' => '2OO Ok',
